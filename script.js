02be0b4fa4395ae61e7e98f07377d3de3929d01d
Alecs_Mz <alex434sassone@gmail.com>
2025-02-27 01:25:27 -0300

    Primer commit de mi sitio web

---
+++ script.js
@@ -0,0 +1,12 @@
+let boton1 = document.getElementById("miBoton1");
+let boton2 = document.getElementById("miBoton2");
+
+// Redirigir al primer enlace (Google) cuando el primer botón sea clickeado
+boton1.addEventListener("click", function() {
+    window.open(f = "https://youtu.be/hOxi-x1wf6M?si=4YUa-oV77BX73tbe","_Blank");
+});
+
+// Redirigir al segundo enlace (YouTube) cuando el segundo botón sea clickeado
+boton2.addEventListener("click", function() {
+    window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD","_Blank");
+});
