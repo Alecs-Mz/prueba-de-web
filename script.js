601ad1fef1d19d817ed1274b70751164de88edf7
Alecs_Mz <alex434sassone@gmail.com>
2025-02-27 02:24:54 -0300

    Actualicé el diseño de la página de inicio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let boton1 = document.getElementById("miBoton1");
-let boton2 = document.getElementById("miBoton2");
+let boton2 = document.getElementById("miBoton2")
+let boton3 = document.getElementById("miBoton3")
 
 // Redirigir al primer enlace (Google) cuando el primer botón sea clickeado
 boton1.addEventListener("click", function() {
@@ -11,8 +12,7 @@ boton2.addEventListener("click", function() {
     window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD","_Blank");
 });
 
-    // Redirigir al segundo enlace (YouTube) cuando el segundo botón sea clickeado
+    // Redirigir al segundo enlace (Instagram) cuando el terccer botón sea clickeado
 boton3.addEventListener("click", function() {
     window.open("https://www.instagram.com/_alex_sassone_/","_Blank");
 });
-s
