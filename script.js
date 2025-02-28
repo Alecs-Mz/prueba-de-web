5369286a7069adefb86276b3957212b60766294c
Alecs_Mz <alex434sassone@gmail.com>
2025-02-28 00:07:31 -0300

    Actualicé el diseño de la página de inicio

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ window.onload = function() {
     let boton3 = document.getElementById("miBoton3");
     let boton4 = document.getElementById("miBoton4");
 
+    // Enlace para el primer botón
     boton1.addEventListener("click", function() {
-        window.open("https://youtu.be/hOxi-x1wf6M?si=4YUa-oV77BX73tbe", "_Blank");
+        window.open("https://youtu.be/hOxi-x1wf6M?si=4YUa-oV77BX73tbe", "_blank");
     });
 
+    // Enlace para el segundo botón
     boton2.addEventListener("click", function() {
-        window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD", "_Blank");
+        window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD", "_blank");
     });
 
+    // Enlace para el tercer botón
     boton3.addEventListener("click", function() {
-        window.open("https://www.instagram.com/_alex_sassone_/", "_Blank");
+        window.open("https://www.instagram.com/_alex_sassone_/", "_blank");
     });
 
+    // Enlace para el cuarto botón
     boton4.addEventListener("click", function() {
-        window.open("https://www.twitch.tv/p44nd0r4", "_Blank");
+        window.open("https://www.twitch.tv/p44nd0r4", "_blank");
     });
 }
