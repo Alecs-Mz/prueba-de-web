67e65ae2a5e2ad8b3c047994c624556d587d5d7e
Alecs_Mz <alex434sassone@gmail.com>
2025-02-28 00:01:02 -0300

    Actualicé el diseño de la página de inicio por completo

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ window.onload = function() {
     let boton3 = document.getElementById("miBoton3");
     let boton4 = document.getElementById("miBoton4");
 
-    // Redirigir al primer enlace (Google) cuando el primer botón sea clickeado
     boton1.addEventListener("click", function() {
         window.open("https://youtu.be/hOxi-x1wf6M?si=4YUa-oV77BX73tbe", "_Blank");
     });
 
-    // Redirigir al segundo enlace (YouTube) cuando el segundo botón sea clickeado
     boton2.addEventListener("click", function() {
         window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD", "_Blank");
     });
 
-    // Redirigir al tercer enlace (Instagram) cuando el tercer botón sea clickeado
     boton3.addEventListener("click", function() {
         window.open("https://www.instagram.com/_alex_sassone_/", "_Blank");
     });
 
-    // Redirigir al cuarto enlace (nuevo) cuando el cuarto botón sea clickeado
     boton4.addEventListener("click", function() {
-        window.open("https://www.ejemplo.com", "_Blank");
+        window.open("https://www.twitch.tv/p44nd0r4", "_Blank");
     });
 }
