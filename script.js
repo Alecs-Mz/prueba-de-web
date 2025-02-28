f168fa5a75be4bf186e406ba76eaf134a33ab42a
Alecs_Mz <alex434sassone@gmail.com>
2025-02-28 00:30:27 -0300

    Actualicé diseño estilo Minecraft

--- script.js
+++ script.js
@@ -4,22 +4,19 @@ window.onload = function() {
     let boton3 = document.getElementById("miBoton3");
     let boton4 = document.getElementById("miBoton4");
 
-    // Redirigir al primer enlace
+    // Redirigir a los enlaces cuando los botones sean clickeados
     boton1.addEventListener("click", function() {
         window.open("https://youtu.be/hOxi-x1wf6M?si=4YUa-oV77BX73tbe", "_blank");
     });
 
-    // Redirigir al segundo enlace
     boton2.addEventListener("click", function() {
         window.open("https://youtu.be/Y8-n6nNoFKc?si=1-O3TjmWTwVGhXoD", "_blank");
     });
 
-    // Redirigir al tercer enlace
     boton3.addEventListener("click", function() {
         window.open("https://www.instagram.com/_alex_sassone_/", "_blank");
     });
 
-    // Redirigir al cuarto enlace
     boton4.addEventListener("click", function() {
         window.open("https://www.twitch.tv/p44nd0r4", "_blank");
     });
